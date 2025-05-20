f98e176c2959110bf66cc1973ceacc01a2124700
maryem gueri <maryem.gueri@gmail.com>
2025-05-20 22:42:51 +0100

    modif final2

--- app.py
+++ app.py
@@ -142,19 +142,9 @@ def reset():
     return redirect(url_for("index"))
 
 
-
 @app.route('/download/<filename>')
 def download_file(filename):
-    full_path = os.path.join(RESULT_FOLDER, filename)
-    if not os.path.exists(full_path):
-        print(f"❌ Fichier demandé introuvable : {filename}")
-        return "Fichier non trouvé", 404
-    return send_from_directory(
-        RESULT_FOLDER,
-        filename,
-        as_attachment=False,                # ✅ ← on garde false pour ouverture dans l'onglet
-        mimetype="application/pdf"          # ✅ ← indispensable pour navigateur
-    )
+    return send_from_directory(RESULT_FOLDER, filename, as_attachment=True)
 
 
 if __name__ == "__main__":
